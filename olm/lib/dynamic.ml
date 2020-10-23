9fe9aa50278b8041ccf96cc8c3bdc3fe47dad960
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-22 23:59:36 -0700

    Clear out old dynamic bindings.

--- olm/lib/dynamic.ml
+++
@@ -1,4 +0,0 @@
-let libolm =
-  Dl.dlopen
-    ~filename:"/home/geoff/GitRepos/ocaml-olm/libolm/build/libolm.so"
-    ~flags:Dl.[ RTLD_NOW ]
--- olm/lib/lib.ml
+++
@@ -1,8 +0,0 @@
-include Crypto
-include Error
-include Inbound_group_session
-include Megolm
-include Olm_h
-include Outbound_group_session
-include Pk
-include Sas
