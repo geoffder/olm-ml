1ea82c13d7ae2f5741703dbaf9d6f23cd52b2922
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-25 23:07:29 -0700

    Begin writing tests.

---
+++ olm/test/account_test.ml
@@ -0,0 +1,19 @@
+open! Core
+open! Olm
+open! Olm.Helpers
+
+let bob = Account.create () |> Result.ok_or_failwith
+
+let%test "get identity keys" =
+  let identity_keys = Account.identity_keys bob |> Result.ok_or_failwith in
+  let algos         = Map.keys identity_keys in
+  List.equal String.equal algos ["curve25519"; "ed25519"]
+
+let%test "get identity keys" =
+  let res =
+    Account.generate_one_time_keys bob 3 >>= fun _ ->
+    Account.one_time_keys bob            >>| fun keys ->
+    Map.find_exn keys "curve25519" |> Map.length
+  in
+  Result.ok_or_failwith res
+  |> ( = ) 3
---
+++ olm/test/utility_test.ml
@@ -0,0 +1,17 @@
+open! Core
+open! Olm
+open! Olm.Helpers
+
+let message = "Hello."
+let utility = Utility.create ()
+let alice   = Account.create () |> Result.ok_or_failwith
+
+let%test "verify account signature" =
+  let res =
+    Account.sign alice message                 >>= fun signature ->
+    Account.identity_keys alice                >>= fun identity_keys ->
+    Map.find_or_error identity_keys "ed25519"
+    |> Result.map_error ~f:Error.to_string_hum >>= fun signing_key ->
+    Utility.ed25519_verify utility signing_key message signature
+  in
+  Result.is_ok res
