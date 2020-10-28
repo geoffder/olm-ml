eabc380265189234013295c58e19089f506d4bd1
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-27 22:33:59 -0700

    IdentityKeys and OneTimeKeys module types.

--- olm/lib/account.ml
+++ olm/lib/account.ml
@@ -2,13 +2,68 @@ open Core
 open Helpers
 open Helpers.ResultInfix
 
+module IdentityKeys : sig
+  type t = private { curve25519 : string
+                   ; ed25519    : string
+                   }
+  val equal     : t -> t -> bool
+  val of_yojson : Yojson.Safe.t -> (t, string) result
+  val to_yojson : t -> Yojson.Safe.t
+  val of_string : string -> (t, string) result
+  val to_string : t -> string
+end = struct
+  type t = { curve25519 : string
+           ; ed25519    : string
+           }
+
+  let equal a b = String.equal a.curve25519 b.curve25519
+                  && String.equal a.ed25519 b.ed25519
+
+  let of_yojson j =
+    YoJs.U.member "curve25519" j |> YoJs.string_of_yojson >>= fun curve25519 ->
+    YoJs.U.member "ed25519" j    |> YoJs.string_of_yojson >>= fun ed25519 ->
+    Result.return { curve25519; ed25519 }
+
+  let to_yojson { curve25519; ed25519 } =
+    `Assoc [ ("curve25519", `String curve25519)
+           ; ("ed25519", `String ed25519)
+           ]
+
+  let of_string s = Yojson.Safe.from_string s |> of_yojson
+  let to_string t = to_yojson t |> Yojson.Safe.to_string
+end
+
+module OneTimeKeys : sig
+  type t = private { curve25519 : (string, string, String.comparator_witness) Map.t }
+  val equal     : t -> t -> bool
+  val of_yojson : Yojson.Safe.t -> (t, string) result
+  val to_yojson : t -> Yojson.Safe.t
+  val of_string : string -> (t, string) result
+  val to_string : t -> string
+end = struct
+  type t = { curve25519 : (string, string, String.comparator_witness) Map.t }
+
+  let equal a b = Map.equal String.equal a.curve25519 b.curve25519
+
+  let of_yojson j =
+    YoJs.U.member "curve25519" j
+    |> YoJs.StringMap.of_yojson YoJs.string_of_yojson
+    >>| fun curve25519 -> { curve25519 }
+
+  let to_yojson { curve25519 } =
+    `Assoc [ ("curve25519", YoJs.(StringMap.to_yojson yo_string curve25519)) ]
+
+  let of_string s = Yojson.Safe.from_string s |> of_yojson
+  let to_string t = to_yojson t |> Yojson.Safe.to_string
+end
+
 type t = { buf : char Ctypes_static.ptr
          ; acc : C.Types.Account.t Ctypes_static.ptr
          }
 
 let size = C.Funcs.account_size () |> size_to_int
 
-let clear = C.Funcs.clear_account
+let clear acc = C.Funcs.clear_account acc |> size_to_result
 
 let check_error t ret =
   size_to_result ret
@@ -57,8 +112,7 @@ let identity_keys t =
   C.Funcs.account_identity_keys t.acc out_buf out_len
   |> check_error t >>= fun _ ->
   string_of_ptr Ctypes.void ~length:(size_to_int out_len) out_buf
-  |> Yojson.Safe.from_string
-  |> YoJs.StringMap.of_yojson YoJs.string_of_yojson
+  |> IdentityKeys.of_string
 
 let sign t msg =
   let msg_buf = string_to_ptr Ctypes.void msg in
@@ -91,9 +145,4 @@ let one_time_keys t =
   C.Funcs.account_one_time_keys t.acc out_buf out_len
   |> check_error t >>= fun _ ->
   string_of_ptr Ctypes.void ~length:(size_to_int out_len) out_buf
-  |> Yojson.Safe.from_string
-  |> YoJs.StringMap.of_yojson YoJs.(StringMap.of_yojson string_of_yojson)
-
-let remove_one_time_keys t session =
-  C.Funcs.remove_one_time_keys t.acc session
-  |> check_error t
+  |> OneTimeKeys.of_string
