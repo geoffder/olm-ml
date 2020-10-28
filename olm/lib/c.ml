e177fe288a4b92ee470a81715cb771b8e3a12cfd
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-28 13:53:16 -0700

    Gen'd interface for C bindings. (WIP clean + doc)

--- olm/lib/c.ml
+++ olm/lib/c.ml
@@ -1,4 +1,6 @@
-module Types = Olm_c_types
+(* module Types = Olm_c_types *)
+module Types =
+  Olm_c_type_descriptions.Descriptions (Olm_c_generated_types)
 
 module Funcs =
   Olm_c_function_descriptions.Descriptions (Olm_c_generated_functions)
