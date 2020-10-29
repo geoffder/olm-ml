69556d6c7be85381a7a6727490d089fe46dff516
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-28 23:34:30 -0700

    Open Core and replace Result.t with result.

--- olm/lib/account.mli
+++ olm/lib/account.mli
@@ -1,19 +1,21 @@
+open Core
+
 module IdentityKeys : sig
   type t = private { curve25519 : string; ed25519 : string; }
   val equal     : t -> t -> bool
-  val of_yojson : Yojson.Safe.t -> (t, [> `YojsonError of string ]) Core.result
+  val of_yojson : Yojson.Safe.t -> (t, [> `YojsonError of string ]) result
   val to_yojson : t -> Yojson.Safe.t
-  val of_string : string -> (t, [> `YojsonError of string ]) Core.result
+  val of_string : string -> (t, [> `YojsonError of string ]) result
   val to_string : t -> string
 end
 
 module OneTimeKeys : sig
   type t = private
-    { curve25519 : (string, string, Core.String.comparator_witness) Core.Map.t }
+    { curve25519 : (string, string, String.comparator_witness) Map.t }
   val equal     : t -> t -> bool
-  val of_yojson : Yojson.Safe.t -> (t, [> `YojsonError of string ]) Core.result
+  val of_yojson : Yojson.Safe.t -> (t, [> `YojsonError of string ]) result
   val to_yojson : t -> Yojson.Safe.t
-  val of_string : string -> (t, [> `YojsonError of string ]) Core.result
+  val of_string : string -> (t, [> `YojsonError of string ]) result
   val to_string : t -> string
 end
 
@@ -23,29 +25,26 @@ type t = { buf : char Ctypes_static.ptr
 
 val size : int
 
-val clear : C.Types.Account.t Ctypes_static.ptr -> (int, [> `OlmError ]) Core.Result.t
+val clear : C.Types.Account.t Ctypes_static.ptr -> (int, [> `OlmError ]) result
 
-val check_error : t -> Unsigned.Size_t.t -> (int, [> OlmError.t ]) Core.Result.t
+val check_error : t -> Unsigned.Size_t.t -> (int, [> OlmError.t ]) result
 
 val alloc : unit -> t
 
-val create : unit -> (t, [> OlmError.t ]) Core.Result.t
+val create : unit -> (t, [> OlmError.t ]) result
 
-val pickle : ?pass:string -> t -> (string, [> OlmError.t ]) Core.Result.t
+val pickle : ?pass:string -> t -> (string, [> OlmError.t ]) result
 
-val from_pickle :
-  ?pass:string -> string -> (t, [> OlmError.t | `ValueError of string ]) Core.Result.t
+val from_pickle : ?pass:string -> string -> (t, [> OlmError.t | `ValueError of string ]) result
 
-val identity_keys :
-  t -> (IdentityKeys.t, [> OlmError.t | `YojsonError of string ]) Core.Result.t
+val identity_keys : t -> (IdentityKeys.t, [> OlmError.t | `YojsonError of string ]) result
 
-val sign : t -> string -> (string, [> OlmError.t ]) Core.Result.t
+val sign : t -> string -> (string, [> OlmError.t ]) result
 
-val max_one_time_keys : t -> (int, [> OlmError.t ]) Core.Result.t
+val max_one_time_keys : t -> (int, [> OlmError.t ]) result
 
-val mark_keys_as_published : t -> (int, [> OlmError.t ]) Core.Result.t
+val mark_keys_as_published : t -> (int, [> OlmError.t ]) result
 
-val generate_one_time_keys : t -> int -> (int, [> OlmError.t ]) Core.Result.t
+val generate_one_time_keys : t -> int -> (int, [> OlmError.t ]) result
 
-val one_time_keys :
-  t -> (OneTimeKeys.t, [> OlmError.t | `YojsonError of string ]) Core.Result.t
+val one_time_keys : t -> (OneTimeKeys.t, [> OlmError.t | `YojsonError of string ]) result
