d40151d3d3d334ec5eb3abb27944474e96dc42b8
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-31 02:38:55 -0700

    Replace last usage of Cryptokit with Nocrypto

--- olm/test/utility_test.ml
+++ olm/test/utility_test.ml
@@ -12,15 +12,17 @@ let%test "sha256" =
   let input1 = "It's a secret to everybody." in
   let input2 = "It's a secret to nobody." in
   let util   = Utility.create () in
-  let cryptokit_hash =
-    let open Cryptokit in
-    hash_string (Hash.sha256 ()) input1
-    |> transform_string (Base64.encode_compact ())
+  let nocrypto_hash =
+    Cstruct.of_string input1
+    |> Nocrypto.Hash.SHA256.digest
+    |> Nocrypto.Base64.encode
+    |> Cstruct.to_string
+    |> String.rstrip ~drop:(Char.equal '=')  (* remove padding from encode *)
   in
   begin
     Utility.sha256 util input1 >>= fun first_hash ->
     Utility.sha256 util input2 >>= fun second_hash ->
     Result.return (first_hash, second_hash)
   end |> function
-  | Ok (h1, h2) -> not (String.equal h1 h2) && String.equal h1 cryptokit_hash
+  | Ok (h1, h2) -> not (String.equal h1 h2) && String.equal h1 nocrypto_hash
   | _ -> false
