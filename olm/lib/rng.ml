dc7d214ff4c67758453a3a242bec186e14275587
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-10-31 02:39:23 -0700

    Improve usage of Nocrypto generated Cstruct

--- olm/lib/rng.ml
+++ olm/lib/rng.ml
@@ -3,18 +3,14 @@ open Ctypes
 
 let () = Nocrypto_entropy_unix.initialize ()
 
-let pick_int () = Nocrypto.(Numeric.Int.of_cstruct_be (Rng.generate 1))
-
-(* The int will always be 0 to 255, thus using exn. *)
-let pick_char _ = pick_int () |> Char.of_int_exn
-
 let char_buf len =
-  List.init ~f:pick_char len
-  |> CArray.of_list char
+  Nocrypto.Rng.generate len
+  |> Cstruct.to_bigarray
+  |> array_of_bigarray array1
   |> CArray.start
 
 let void_buf len = char_buf len |> to_voidp
 
 let uint8_buf len = char_buf len |> (coerce (ptr char) (ptr uint8_t))
 
-let string len = List.init ~f:pick_char len |> String.of_char_list
+let string len = char_buf len |> Helpers.string_of_ptr char ~length:len
